3b977a43fbe5690fb76f0071a82cf9cc0f9cf9d4
Robi02 <robi9202@gmail.com>
2018-10-30 09:08:11 +0900

    rblog 추가.

---
+++ Study/C/2.rblib/rblib/include/common_define.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_DEFINE_H__
+#define __COMMON_DEFINE_H__
+
+#define _WINDOWS_OS_
+// #define _LINUX_OS_
+
+#define _X86_SYSTEM_
+// #define _X64_SYSTEM_
+
+#define _LITTLE_ENDIAN_CPU_
+// #define _BIG_ENDIAN_CPU_
+
+#endif
